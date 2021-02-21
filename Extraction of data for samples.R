3203923d1408a702099d7710fff0354c5c28108d
Ahmed Adel Aboushanab <35786125+AhmedAboushanab@users.noreply.github.com>
2021-02-22 00:51:32 +0200

    Update Extraction of data for samples.R

--- Extraction of data for samples.R
+++ Extraction of data for samples.R
@@ -4,38 +4,25 @@ library(ExperimentHub)
 library(stringr)
 
 #getting the dataset
-Dataset <- curatedMetagenomicData("PasolliE_2018.metaphlan_bugs_list.stool", dryrun = FALSE)
+Dataset <- curatedMetagenomicData("Dataset_name", dryrun = FALSE)
 dataset2 <- mergeData(Dataset)
 experimentData(dataset2)
-JieZ <- pData(dataset2)
-write.csv(JieZ, file = "PasolliE_2018_dataset.csv", row.names = F)
-JieZ_results <- exprs(dataset2)
-write.csv(JieZ_results, file = "PasolliE_2018_relative abundance_dataset.csv", row.names = T)
+Metadata <- pData(dataset2)
+write.csv(JieZ, file = "Metadata_dataset name.csv", row.names = F)
+Relative_Abundance_results <- exprs(dataset2)
+write.csv(JieZ_results, file = "Relative Abundance file.csv", row.names = T)
 
 #filtering healthy samples data
-subdata <- read.csv("VatanenT_2016_relative abundance_dataset.csv", header = FALSE)
-healthy <- read.csv("Backhed.csv", header = FALSE)
+subdata <- read.csv("Relative Abundance file.csv", header = FALSE)
+healthy <- read.csv("File with sample ids.csv", header = FALSE)
 
-#modifying column names in subdata
-#subdata2 <- list()
-#for(e in 1:ncol(subdata)){
-#  h = subdata[1 , e]
-#  if(str_detect(h, "AsnicarF_2017.metaphlan_bugs_list.stool:") == TRUE){
-#    new = gsub("AsnicarF_2017.metaphlan_bugs_list.stool:", "", h) #removes a pattern from file name 
-#    subdata2 = append(subdata2, new)
-#  }else{
-#    print("FALSE")
-#  }
-#}
 
-#print(subdata2)
-#head(subdata)
+#healthy data
+subdata3 <- read.csv("Realative Abundance file.csv", header = FALSE)
+write.csv(subdata3[1], file = "Taxa availabe.csv", row.names = FALSE)#change name each time for different datasets
 
-#healthy data ISA
-subdata3 <- read.csv("VatanenT_2016_relative abundance_dataset.csv", header = FALSE)
-write.csv(subdata3[1], file = "VatanenT_2016.csv", row.names = FALSE)#change name
-
-healthydata <- read.csv("VatanenT_2016.csv", header = FALSE) #change name
+#Cgecking the ids for healthy samples agains relative abundacne data and add their information to an empty list
+healthydata <- read.csv("Realative Abundance file.csv", header = FALSE) #change name each time for different datasets
 for(f in 1:nrow(healthy)){
   for(e in 1:ncol(subdata)){
     g = healthy[f, 1]
